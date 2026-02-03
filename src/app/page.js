cd6716de19f16abce9fa731c0bb21334ef012e30
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-02-03 16:22:24 +0545

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div className="px-3 mt-3">
-    <div className="w-full h-[500px] relative ">
+    <div className="w-full h-[600px] relative ">
   <Image src="/hero.jpg" fill alt="heroimage" className="object-cover rounded-2xl"/>
   </div>
   </div>

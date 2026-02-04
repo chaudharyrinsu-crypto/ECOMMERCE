2eba3a63dac9b2a3540fa4179b3f8a80ab513fff
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-02-04 13:11:13 +0545

    changes

--- src/app/hoverItems/Collection.jsx
+++ src/app/hoverItems/Collection.jsx
@@ -6,30 +6,35 @@ import Link from 'next/link'
 const Collection = () => {
   return (
     <>
-      <section className='absolute top-0 left-0 z-50 bg-[#ECE9E2] w-full py-40'>
+      <section className='absolute top-0 left-0 z-50 bg-[#ECE9E2] w-full py-20'>
         <div className='max-w-[1320px] m-auto'>
-            <div className='grid grid-cols-4 gap-3 text-[14px]'>
-            <div>
+            <div className='bg-[#E0DACF] py-3 rounded-xl  space-x-6 text-[14px] font-medium text-center'>
+                                <Link href='/'>Shoes</Link>
+                                <Link href='/'>Socks & Apparel</Link>
+                                <Link href='/'>Sale</Link>
+                            </div>
+            <div className='grid grid-cols-4 text-[14px]'>
+            <div className='py-2'>
                 {allCollection.map(a=>(
-                    <Link className='uppercase flex mt-6 font-semibold' key={a} href='/'>{a}</Link>
+                    <Link className='uppercase flex mt-6 font-medium hover:underline' key={a} href='/'>{a}</Link>
                 ))}
             </div>
-            <div>
-                <h5 className='mt-6'>Men's Shoes</h5>
+            <div className='py-2'>
+                <Link href='/' className='mt-6 inline-block font-medium uppercase hover:underline'>Men's Shoes</Link>
                 {menShoes.map(a=>(
-                    <Link className='text-[#575757] flex mt-3' key={a} href='/'>{a}</Link>
+                    <Link className='text-[#575757] flex mt-3 hover:underline' key={a} href='/'>{a}</Link>
                 ))}
             </div>
-            <div>
-                 <h5 className='mt-6'>Customer Favorites</h5>
+            <div className='py-2'>
+                 <h5 className='mt-6 uppercase font-medium'>Customer Favorites</h5>
                 {customerFavorites.map(a=>(
-                    <Link className='text-[#575757] flex mt-3' key={a} href='/'>{a}</Link>
+                    <Link className='text-[#575757] flex mt-3 hover:underline' key={a} href='/'>{a}</Link>
                 ))}
             </div>
-            <div>
-                <h5 className='mt-6'>Top Add-Ons</h5>
+            <div className='py-2'>
+                <h5 className='mt-6 uppercase font-medium'>Top Add-Ons</h5>
                 {topAddOns.map(a=>(
-                    <Link className='text-[#575757] flex mt-3' key={a} href='/'>{a}</Link>
+                    <Link className='text-[#575757] flex mt-3 hover:underline' key={a} href='/'>{a}</Link>
                 ))}
             </div>
            
